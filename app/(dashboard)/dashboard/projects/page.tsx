b1b7aa1effc297e488e30d1de4fc2c91fc6d16a0
Justin Neubert <justin.neubert@kuehne-nagel.com>
2024-06-11 08:59:42 +0200

    reimplement loading skeleton for projectlist

--- app/(dashboard)/dashboard/projects/page.tsx
+++ app/(dashboard)/dashboard/projects/page.tsx
@@ -1,5 +1,7 @@
 import BreadCrumb from '@/components/breadcrumb';
+import { Suspense } from 'react'
 import { ProjectList } from "@/components/dashboard/projects/projectList";
+import { ProjectListLoading } from "@/components/dashboard/projects/projectListLoading";
 
 const breadcrumbItems = [{ title: 'Projects', link: '/dashboard/projects' }];
 export default function page() {
@@ -7,7 +9,9 @@ export default function page() {
     <>
       <div className="flex-1 space-y-4  p-4 pt-6 md:p-8">
         <BreadCrumb items={breadcrumbItems} />
-        <ProjectList />
+        <Suspense fallback={<ProjectListLoading />}>
+          <ProjectList />
+        </Suspense>
       </div>
     </>
   );
