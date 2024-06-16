21305cb5eedc7f08cf3f5f6af67aa10c8ae7bc0c
JustNZ <neubert_justin@outlook.com>
2024-06-16 22:41:49 +0200

    pre project card design

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,7 +1,18 @@
+import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/react";
+
 import { ProjectsList } from "@/components/dashboard/projects/list";
 
 export default function DashboardProjectsPage() {
   return (
-    <ProjectsList />
+    <>
+      <Breadcrumbs>
+        <BreadcrumbItem>Home</BreadcrumbItem>
+        <BreadcrumbItem>Music</BreadcrumbItem>
+        <BreadcrumbItem>Artist</BreadcrumbItem>
+        <BreadcrumbItem>Album</BreadcrumbItem>
+        <BreadcrumbItem>Song</BreadcrumbItem>
+      </Breadcrumbs>
+      <ProjectsList />
+    </>
   );
 }
--- components/dashboard/projects/list.tsx
+++ components/dashboard/projects/list.tsx
@@ -1,12 +1,71 @@
 import React from "react";
-import { Card, CardBody } from "@nextui-org/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Code,
+} from "@nextui-org/react";
+
+import { subtitle } from "@/components/primitives";
 
 export function ProjectsList() {
   return (
-        <Card>
-          <CardBody>
-            <p>Make beautiful websites regardless of your design experience.</p>
-          </CardBody>
-        </Card>
+    <main>
+      <h1 className={subtitle()} style={{ color: "violet" }}>
+        Projects
+      </h1>
+      <Divider className="mb-4" />
+      <div className="grid grid-cols-2 gap-4">
+        <div className="col-span-1">
+          <Card className="hover:cursor-pointer hover:shadow hover:shadow-primary shadow shadow-primary-200">
+            <CardHeader className="justify-between">
+              <p className="text-md">JustLab</p>
+              <Code color="secondary">123123-123123123-12312313-13123</Code>
+            </CardHeader>
+            <Divider />
+            <CardBody>
+              <p>
+                Make beautiful websites regardless of your design experience.
+              </p>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <p className="text-small text-default-500">
+                Last Modified: {new Date().toDateString()}
+              </p>
+            </CardFooter>
+          </Card>
+        </div>
+        <div className="col-span-1">
+          <Card>
+            <CardBody>
+              <p>
+                Make beautiful websites regardless of your design experience.
+              </p>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-span-1">
+          <Card>
+            <CardBody>
+              <p>
+                Make beautiful websites regardless of your design experience.
+              </p>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-span-1">
+          <Card>
+            <CardBody>
+              <p>
+                Make beautiful websites regardless of your design experience.
+              </p>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+    </main>
   );
 }
