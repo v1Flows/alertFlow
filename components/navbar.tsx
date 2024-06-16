635facdce2ab2e3bc368e62d5b66046b57146f15
JustNZ <neubert_justin@outlook.com>
2024-06-16 20:41:00 +0200

    new navbar with dropdown

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,19 @@
 import {
   Navbar as NextUINavbar,
+  NavbarBrand,
   NavbarContent,
+  NavbarItem,
   NavbarMenu,
   NavbarMenuToggle,
-  NavbarBrand,
-  NavbarItem,
   NavbarMenuItem,
-} from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
+  Link,
+  Button,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu
+} from "@nextui-org/react";
 import { Kbd } from "@nextui-org/kbd";
-import { Link } from "@nextui-org/link";
 import { Input } from "@nextui-org/input";
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
@@ -20,14 +24,11 @@ import Login from "@/components/auth/login";
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
-  TwitterIcon,
   GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
   SearchIcon,
   Logo,
-  LoginIcon,
 } from "@/components/icons";
+import DashboardMenu from "./navbar/dashboard";
 
 export const Navbar = () => {
   const searchInput = (
@@ -53,29 +54,38 @@ export const Navbar = () => {
 
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
+      <NavbarBrand as="li" className="gap-1 max-w-fit">
+        <Logo />
+        <p className="font-bold text-inherit">AlertFlow</p>
+      </NavbarBrand>
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">AlertFlow</p>
-          </NextLink>
-        </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </ul>
+        <NavbarItem isActive>
+          <Link
+            href="/"
+            aria-current="page"
+          >
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <DashboardMenu />
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            color="foreground"
+            href="/faq"
+          >
+            FAQ
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            color="foreground"
+            href="/support"
+          >
+            Support
+          </Link>
+        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent
