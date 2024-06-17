d19166ca684fe588c169950f17b9575dc8efa074
Justin Neubert <justin.neubert@kuehne-nagel.com>
2024-06-17 14:26:46 +0200

    add middleware

---
+++ lib/auth/updateSession.ts
@@ -0,0 +1,35 @@
+"use server";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function updateSession(request: NextRequest) {
+  const session = cookies().get("session")?.value;
+
+  if (!session) return;
+
+  // Refresh the token
+  const response = await fetch("http://localhost:8080/api/token/refresh", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: session,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to refresh token");
+  }
+
+  const data = await response.json();
+
+  const res = NextResponse.next();
+
+  res.cookies.set({
+    name: "session",
+    value: data.token,
+    expires: new Date(data.expires_at * 1000),
+    httpOnly: true,
+  });
+
+  return res;
+}
