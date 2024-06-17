56e2f9fe97cb3ce756777304b76b2cd006dbd874
Justin Neubert <justin.neubert@kuehne-nagel.com>
2024-06-17 14:42:23 +0200

    fix sessions

--- lib/setSession.ts
+++ lib/setSession.ts
@@ -2,8 +2,8 @@
 
 import { cookies } from "next/headers";
 
-export async function setSession(token: string, user: any, expires_at: string) {
-  const expires = new Date(expires_at);
+export async function setSession(token: string, user: any, expires_at: number) {
+  const expires = new Date(expires_at * 1000);
 
   cookies().set({
     name: "session",
@@ -15,6 +15,7 @@ export async function setSession(token: string, user: any, expires_at: string) {
   cookies().set({
     name: "user",
     value: JSON.stringify(user),
+    expires,
     httpOnly: true,
   });
 }
