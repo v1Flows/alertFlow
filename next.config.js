a93ebc7b27e3e50aa791b0ff30732c79b1897bb9
JustNZ <neubert_justin@outlook.com>
2024-06-20 20:00:20 +0200

    remove pwa again

--- next.config.js
+++ next.config.js
@@ -1,22 +1,3 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  dest: "public",
-  fallbacks: {
-    //image: "/static/images/fallback.png",
-    document: "/", // if you want to fallback to a custom page rather than /_offline
-    // font: '/static/font/fallback.woff2',
-    // audio: ...,
-    // video: ...,
-  },
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-  // ... other options you like
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
@@ -28,4 +9,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
