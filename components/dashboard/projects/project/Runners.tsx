6fb3e7c58cc172fad3d0be55f979dfc8fb876ff5
JustNZ <neubert_justin@outlook.com>
2024-06-27 23:41:50 +0200

    minor runner changes with heartbeat

--- components/dashboard/projects/project/Runners.tsx
+++ components/dashboard/projects/project/Runners.tsx
@@ -21,8 +21,13 @@ import {
 import { useRouter } from "next/navigation";
 import { Toaster, toast } from "sonner";
 import React from "react";
+import TimeAgo from "react-timeago";
 
-import { VerticalDotsIcon, DeleteDocumentIcon, CopyDocumentIcon } from "@/components/icons";
+import {
+  VerticalDotsIcon,
+  DeleteDocumentIcon,
+  CopyDocumentIcon,
+} from "@/components/icons";
 import DeleteProjectRunner from "@/lib/fetch/project/DELETE/DeleteRunner";
 import AddRunnerModal from "@/components/dashboard/projects/project/modals/AddRunner";
 
@@ -66,6 +71,30 @@ export default function Runners({ runners, project }: any) {
     }
   }
 
+  function heartbeatColor(runner: any) {
+    var timeAgo =
+      (new Date(runner.last_heartbeat.Time).getTime() - Date.now()) / 1000;
+
+    if (timeAgo < 0 && timeAgo > -30) {
+      return "success";
+    } else if (timeAgo <= -30 && timeAgo > -60) {
+      return "warning";
+    } else if (timeAgo <= -60) {
+      return "danger";
+    }
+  }
+
+  function heartbeatStatus(runner: any) {
+    var timeAgo =
+      (new Date(runner.last_heartbeat.Time).getTime() - Date.now()) / 1000;
+
+    if (timeAgo < 0 && timeAgo > -30) {
+      return true;
+    } else if (timeAgo <= -30) {
+      return false;
+    }
+  }
+
   return (
     <main>
       <Toaster richColors position="bottom-center" />
@@ -84,13 +113,20 @@ export default function Runners({ runners, project }: any) {
                     <p className="text-md">{runner.name}</p>
                     <p className="text-sm text-default-500">{runner.id}</p>
                   </div>
-                  <div className="flex items-center gap-2">
+                  <div className="flex flex-wrap items-center gap-2">
                     <Chip
                       color={runner.registered ? "success" : "danger"}
                       size="sm"
                       variant="dot"
                     >
                       {runner.registered ? "Registered" : "Unregistered"}
+                    </Chip>
+                    <Chip
+                      color={heartbeatColor(runner)}
+                      size="sm"
+                      variant="flat"
+                    >
+                      {heartbeatStatus(runner) ? "Healthy" : "Unhealthy"}
                     </Chip>
                     <div className="relative flex justify-end items-center gap-2">
                       <Dropdown backdrop="opaque">
@@ -135,8 +171,10 @@ export default function Runners({ runners, project }: any) {
                     <p className="text-sm">Active:</p>
                     <p className="text-sm">{runner.active ? "Yes" : "No"}</p>
                     <p className="text-sm">Last Heartbeat:</p>
-                    <p className="text-sm">
-                      {new Date(runner.last_heartbeat.Time).toLocaleString()}
+                    <p
+                      className={"text-" + heartbeatColor(runner) + " text-sm"}
+                    >
+                      <TimeAgo date={runner.last_heartbeat.Time} />
                     </p>
                   </div>
                 </CardBody>
@@ -154,24 +192,37 @@ export default function Runners({ runners, project }: any) {
               (runner: any) =>
                 runner.alertflow_runner === true && (
                   <Card key={runner.id}>
-                    <CardHeader>
+                    <CardHeader className="justify-between items-center">
                       <div>
                         <p className="text-md">{runner.name}</p>
                         <p className="text-sm text-default-500">{runner.id}</p>
+                      </div>
+                      <div className="flex flex-wrap items-center gap-2">
+                        <Chip
+                          color={heartbeatColor(runner)}
+                          size="sm"
+                          variant="flat"
+                        >
+                          {heartbeatStatus(runner) ? "Healthy" : "Unhealthy"}
+                        </Chip>
                       </div>
                     </CardHeader>
                     <Divider />
                     <CardBody>
                       <div className="grid grid-cols-2 grid-rows-3 items-center justify-center">
                         <p className="text-sm">Version:</p>
-                        <p>{runner.runner_version}</p>
+                        <p className="text-sm">{runner.runner_version}</p>
                         <p className="text-sm">Active:</p>
-                        <p>{runner.active ? "Yes" : "No"}</p>
+                        <p className="text-sm">
+                          {runner.active ? "Yes" : "No"}
+                        </p>
                         <p className="text-sm">Last Heartbeat:</p>
-                        <p>
-                          {new Date(
-                            runner.last_heartbeat.Time,
-                          ).toLocaleString()}
+                        <p
+                          className={
+                            "text-" + heartbeatColor(runner) + " text-sm"
+                          }
+                        >
+                          <TimeAgo date={runner.last_heartbeat.Time} />
                         </p>
                       </div>
                     </CardBody>
