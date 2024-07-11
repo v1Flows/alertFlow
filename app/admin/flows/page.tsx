20cfa0d9ef1e8d899ac5082c9f0d534012a07c0c
JustNZ <neubert_justin@outlook.com>
2024-07-11 18:01:49 +0200

    #28 | flows page

--- app/admin/flows/page.tsx
+++ app/admin/flows/page.tsx
@@ -1,9 +1,14 @@
 import { FlowsList } from "@/components/admin/flows/list";
+import AdminGetFlows from "@/lib/fetch/admin/flows";
+import AdminGetProjects from "@/lib/fetch/admin/projects";
 
-export default function AdminFlowsPage() {
+export default async function AdminFlowsPage() {
+  const flows = await AdminGetFlows();
+  const projects = await AdminGetProjects();
+
   return (
     <>
-      <FlowsList />
+      <FlowsList flows={flows} projects={projects} />
     </>
   );
 }
--- components/admin/flows/list.tsx
+++ components/admin/flows/list.tsx
@@ -1,4 +1,238 @@
-export function FlowsList() {
+"use client";
+import React from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Chip,
+  Divider,
+  Snippet,
+  Dropdown,
+  DropdownTrigger,
+  Button,
+  DropdownMenu,
+  DropdownItem,
+  DropdownSection,
+  cn,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  Input,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+import {
+  DeleteDocumentIcon,
+  EditDocumentIcon,
+  EyeIcon,
+  LockIcon,
+  VerticalDotsIcon,
+} from "@/components/icons";
+import ChangeProjectStatus from "@/lib/fetch/project/PUT/ChangeProjectStatus";
+
+export function FlowsList({ flows, projects }: any) {
+  const router = useRouter();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [disableReason, setDisableReason] = React.useState("");
+  const [isDisableLoading, setIsDisableLoading] = React.useState(false);
+
+  const [projectID, setProjectID] = React.useState("");
+  const [disableProject, setDisableProject] = React.useState(false);
+
+  React.useEffect(() => {
+    if (projectID !== "" && !disableProject) {
+      changeProjectStatus();
+    }
+  }, [projectID, disableProject]);
+
+  function changeProjectStatusModal(projectID: string, disabled: boolean) {
+    setProjectID(projectID);
+    setDisableProject(disabled);
+
+    if (disabled) {
+      onOpenChange();
+    }
+  }
+
+  async function changeProjectStatus() {
+    if (!disableProject) {
+      const res = await ChangeProjectStatus(projectID, disableProject, "");
+
+      if (!res.error) {
+        setProjectID("");
+        router.refresh();
+        toast.success("Project status updated successfully");
+      } else {
+        router.refresh();
+        toast.error("Failed to update project status");
+      }
+    } else {
+      setIsDisableLoading(true);
+      const res = await ChangeProjectStatus(
+        projectID,
+        disableProject,
+        disableReason,
+      );
+
+      if (!res.error) {
+        setDisableReason("");
+        setProjectID("");
+        setDisableProject(false);
+        setIsDisableLoading(false);
+        onOpenChange();
+        router.refresh();
+        toast.success("Project status updated successfully");
+      } else {
+        setIsDisableLoading(false);
+        router.refresh();
+        toast.error("Failed to update project status");
+      }
+    }
+  }
+
+  const iconClasses =
+    "text-xl text-default-500 pointer-events-none flex-shrink-0";
+
+  const renderCell = React.useCallback((flow: any, columnKey: any) => {
+    const cellValue = flow[columnKey];
+
+    switch (columnKey) {
+      case "id":
+        return <p>{flow.id}</p>;
+      case "name":
+        return (
+          <div>
+            <p>{flow.name}</p>
+            <p className="text-sm text-default-500">{flow.description}</p>
+          </div>
+        );
+      case "project_id":
+        return (
+          <div>
+            <p>{projects.find((p: any) => p.id === flow.project_id).name}</p>
+            <p className="text-xs text-default-400">{flow.project_id}</p>
+          </div>
+        );
+      case "disabled":
+        return (
+          <div>
+            <Chip
+              className="capitalize"
+              color={flow.disabled ? "danger" : "success"}
+              radius="sm"
+              size="sm"
+              variant="flat"
+            >
+              {flow.disabled ? "Disabled" : "Active"}
+            </Chip>
+            {flow.disabled && (
+              <p className="text-sm text-default-400">{flow.disabled_reason}</p>
+            )}
+          </div>
+        );
+      case "a_actions":
+        return <p>{flow.actions.length}</p>;
+      case "created_at":
+        return new Date(flow.created_at).toLocaleString("de-DE");
+      case "updated_at":
+        return new Date(flow.updated_at).toLocaleString("de-DE");
+      case "actions":
+        return (
+          <div className="relative flex justify-center items-center gap-2">
+            <Dropdown>
+              <DropdownTrigger>
+                <Button isIconOnly size="sm" variant="light">
+                  <VerticalDotsIcon className="text-default-300" />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu variant="faded">
+                <DropdownSection showDivider title="Actions">
+                  <DropdownItem
+                    key="view"
+                    className="text-primary"
+                    color="primary"
+                    description="Take a look on this flow"
+                    startContent={
+                      <EyeIcon className={cn(iconClasses, "text-primary")} />
+                    }
+                  >
+                    View
+                  </DropdownItem>
+                </DropdownSection>
+                <DropdownSection title="Edit Zone">
+                  <DropdownItem
+                    key="edit"
+                    className="text-warning"
+                    color="warning"
+                    description="Apply changes to this flow"
+                    startContent={
+                      <EditDocumentIcon
+                        className={cn(iconClasses, "text-warning")}
+                      />
+                    }
+                  >
+                    Edit
+                  </DropdownItem>
+                  {flow.disabled && (
+                    <DropdownItem
+                      key="disable"
+                      className="text-success"
+                      color="success"
+                      description="Disable access to this flow for members"
+                      startContent={
+                        <LockIcon className={cn(iconClasses, "text-success")} />
+                      }
+                      onClick={() => changeProjectStatusModal(flow.id, false)}
+                    >
+                      Enable
+                    </DropdownItem>
+                  )}
+                  {!flow.disabled && (
+                    <DropdownItem
+                      key="disable"
+                      className="text-danger"
+                      color="danger"
+                      description="Disable access to this flow for members"
+                      startContent={
+                        <LockIcon className={cn(iconClasses, "text-danger")} />
+                      }
+                      onClick={() => changeProjectStatusModal(flow.id, true)}
+                    >
+                      Disable
+                    </DropdownItem>
+                  )}
+                </DropdownSection>
+                <DropdownSection title="Danger Zone">
+                  <DropdownItem
+                    key="delete"
+                    className="text-danger"
+                    color="danger"
+                    description="Permanently delete this flow"
+                    startContent={
+                      <DeleteDocumentIcon
+                        className={cn(iconClasses, "text-danger")}
+                      />
+                    }
+                  >
+                    Delete
+                  </DropdownItem>
+                </DropdownSection>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+
   return (
     <main>
       <div className="flex items-center justify-between">
@@ -7,6 +241,90 @@ export function FlowsList() {
           <p className="text-2xl mb-0">|</p>
           <p className="text-2xl mb-0">Flows</p>
         </div>
+      </div>
+      <Divider className="my-4" />
+      <div>
+        <Table aria-label="Example table with custom cells">
+          <TableHeader>
+            <TableColumn key="id" align="start">
+              ID
+            </TableColumn>
+            <TableColumn key="name" align="start">
+              NAME
+            </TableColumn>
+            <TableColumn key="project_id" align="start">
+              PROJECT
+            </TableColumn>
+            <TableColumn key="runner_id" align="start">
+              RUNNER
+            </TableColumn>
+            <TableColumn key="disabled" align="start">
+              STATUS
+            </TableColumn>
+            <TableColumn key="a_actions" align="start">
+              AVL. ACTIONS
+            </TableColumn>
+            <TableColumn key="created_at" align="start">
+              CREATED AT
+            </TableColumn>
+            <TableColumn key="updated_at" align="start">
+              UPDATED AT
+            </TableColumn>
+            <TableColumn key="actions" align="center">
+              ACTIONS
+            </TableColumn>
+          </TableHeader>
+          <TableBody emptyContent={"No rows to display."} items={flows}>
+            {(item: any) => (
+              <TableRow key={item.id}>
+                {(columnKey) => (
+                  <TableCell>{renderCell(item, columnKey)}</TableCell>
+                )}
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div>
+        <Modal
+          isOpen={isOpen}
+          placement="top-center"
+          onOpenChange={onOpenChange}
+        >
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-wrap items-center justify-center gap-2 font-bold text-danger">
+                  <LockIcon /> Disable Project
+                </ModalHeader>
+                <ModalBody>
+                  <Snippet hideCopyButton hideSymbol>
+                    <span>ID: {projectID}</span>
+                  </Snippet>
+                  <Input
+                    label="Disable Reason"
+                    placeholder="Enter the reason for disabling this project"
+                    value={disableReason}
+                    variant="bordered"
+                    onValueChange={setDisableReason}
+                  />
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="default" variant="flat" onPress={onClose}>
+                    Cancel
+                  </Button>
+                  <Button
+                    color="danger"
+                    isLoading={isDisableLoading}
+                    onPress={changeProjectStatus}
+                  >
+                    Disable
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
     </main>
   );
