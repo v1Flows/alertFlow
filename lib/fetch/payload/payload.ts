b50f3bba7ff95175f6d467d8ac7fe24c049e08bd
JustNZ <neubert_justin@outlook.com>
2024-07-22 18:48:15 +0200

    add payload fetch

---
+++ lib/fetch/payload/payload.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export default async function GetPayload(payloadID: any) {
+  "use client";
+  const cookieStore = cookies();
+  const token = cookieStore.get("session")?.value;
+
+  try {
+    const headers = new Headers();
+
+    headers.append("Content-Type", "application/json");
+    if (token) {
+      headers.append("Authorization", token);
+    }
+    const res = await fetch(
+      `${process.env.API_ENDPOINT}/payloads/${payloadID}`,
+      {
+        method: "GET",
+        headers: headers,
+      },
+    );
+    const data = await res.json();
+
+    return data.payload;
+  } catch (error) {
+    return { error: "Failed to fetch data" };
+  }
+}
