7127c37ce915baf1e8e30fd9fc7c81a52ce40250
JustNZ <neubert_justin@outlook.com>
2024-07-22 19:00:31 +0200

    bump version to 0.8.0-beta

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "AlertFlow",
   description: "Monitoring Automation Platform.",
-  version: "0.7.0-beta",
+  version: "0.8.0-beta",
   links: {
     github: "https://github.com/AlertFlow",
     docs: "https://docs.alertflow.org",
