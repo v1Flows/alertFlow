14e3b97dfb10ef1b0465d8fb5d9ed39b90258f80
JustNZ <neubert_justin@outlook.com>
2024-08-21 22:15:53 +0200

    chore: Update site version to 0.19.1-beta

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "AlertFlow",
   description: "Monitoring Automation Platform.",
-  version: "0.19.0-beta",
+  version: "0.19.1-beta",
   links: {
     github: "https://github.com/AlertFlow",
     docs: "https://docs.alertflow.org",
