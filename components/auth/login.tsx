db63303309b56255e40ef20e4109c8933b28e4cf
JustNZ <neubert_justin@outlook.com>
2024-08-21 10:40:38 +0200

    chore: Add new signup and login pages

--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -1,25 +1,14 @@
 /* eslint-disable no-undef */
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import {
   Dropdown,
   DropdownTrigger,
   DropdownMenu,
   DropdownItem,
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
   Button,
-  useDisclosure,
-  Checkbox,
-  Input,
-  Link,
   Avatar,
   User,
-  Card,
-  CardHeader,
   Tooltip,
 } from "@nextui-org/react";
 import { LogInIcon, UserPlusIcon } from "lucide-react";
@@ -27,52 +16,12 @@ import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 import { Logout } from "@/lib/logout";
-import {
-  MailIcon,
-  LockIcon,
-  InfoIcon,
-  CopyDocumentIcon,
-} from "@/components/icons";
-import { setSession } from "@/lib/setSession";
-import { IconWrapper } from "@/lib/IconWrapper";
-import LoginAPI from "@/lib/auth/login";
-
-import SignUpModal from "../functions/auth/signUp";
+import { CopyDocumentIcon } from "@/components/icons";
 
 export default function Login({ user, session, showSignUp, settings }: any) {
   const router = useRouter();
 
-  const signUpModal = useDisclosure();
-
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const [isLoginLoading, setIsLoginLoading] = useState(false);
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [rememberMe, setRememberMe] = useState(false);
-
-  const [error, setError] = useState(false);
-  const [errorText, setErrorText] = useState("");
-
   const userData = user;
-
-  async function onLogin() {
-    setIsLoginLoading(true);
-    setError(false);
-    setErrorText("");
-
-    const res = await LoginAPI(email, password);
-
-    if (!res.error) {
-      setSession(res.token, res.user, res.expires_at);
-      setIsLoginLoading(false);
-      onOpenChange();
-    } else {
-      setIsLoginLoading(false);
-      setError(true);
-      setErrorText(res.error);
-    }
-  }
 
   async function LogoutHandler() {
     await Logout();
@@ -151,7 +100,7 @@ export default function Login({ user, session, showSignUp, settings }: any) {
               color="primary"
               startContent={<LogInIcon />}
               variant="flat"
-              onPress={onOpen}
+              onPress={() => router.push("/auth/login")}
             >
               Login
             </Button>
@@ -160,7 +109,7 @@ export default function Login({ user, session, showSignUp, settings }: any) {
                 color="secondary"
                 startContent={<UserPlusIcon />}
                 variant="flat"
-                onPress={() => signUpModal.onOpen()}
+                onPress={() => router.push("/auth/signup")}
               >
                 Sign Up
               </Button>
@@ -194,97 +143,8 @@ export default function Login({ user, session, showSignUp, settings }: any) {
               </Tooltip>
             )}
           </div>
-          <Modal
-            isDismissable={false}
-            isOpen={isOpen}
-            placement="center"
-            onOpenChange={onOpenChange}
-          >
-            <ModalContent>
-              {(onClose: any) => (
-                <>
-                  <ModalHeader className="flex flex-col gap-1">
-                    Log in
-                  </ModalHeader>
-                  <ModalBody>
-                    {error && (
-                      <Card className="border border-danger-300 border-2">
-                        <CardHeader className="justify-start gap-2 items-center">
-                          <IconWrapper className="bg-danger/10 text-danger">
-                            <InfoIcon className="text-lg" />
-                          </IconWrapper>
-                          <p className="text-md font-bold text-danger capitalize">
-                            {errorText}
-                          </p>
-                        </CardHeader>
-                      </Card>
-                    )}
-                    <Input
-                      required
-                      endContent={
-                        <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                      }
-                      label="Email"
-                      name="email"
-                      placeholder="Enter your email"
-                      type="email"
-                      value={email}
-                      variant="bordered"
-                      onValueChange={setEmail}
-                    />
-                    <Input
-                      required
-                      endContent={
-                        <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                      }
-                      label="Password"
-                      name="password"
-                      placeholder="Enter your password"
-                      type="password"
-                      value={password}
-                      variant="bordered"
-                      onValueChange={setPassword}
-                    />
-                    <div className="flex py-2 px-1 justify-between">
-                      <Checkbox
-                        classNames={{
-                          label: "text-small",
-                        }}
-                        isSelected={rememberMe}
-                        onValueChange={setRememberMe}
-                      >
-                        Remember me
-                      </Checkbox>
-                      <Link color="primary" href="#" size="sm">
-                        Forgot password?
-                      </Link>
-                    </div>
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button
-                      fullWidth
-                      color="danger"
-                      variant="flat"
-                      onPress={onClose}
-                    >
-                      Close
-                    </Button>
-                    <Button
-                      fullWidth
-                      color="primary"
-                      isLoading={isLoginLoading}
-                      onPress={onLogin}
-                    >
-                      Sign in
-                    </Button>
-                  </ModalFooter>
-                </>
-              )}
-            </ModalContent>
-          </Modal>
         </>
       )}
-      <SignUpModal disclosure={signUpModal} />
     </>
   );
 }
