cb5a0806edfd6f5c42efaac06e049ebdb1b8555b
JustNZ <neubert_justin@outlook.com>
2024-08-31 23:58:24 +0200

    flows: actions redesign part 2

--- components/dashboard/flows/flow/actions.tsx
+++ components/dashboard/flows/flow/actions.tsx
@@ -13,11 +13,23 @@ import {
   TableRow,
   useDisclosure,
 } from "@nextui-org/react";
-import React from "react";
+import React, { useEffect } from "react";
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 import DeleteActionModal from "@/components/functions/flows/deleteAction";
 import EditActionModal from "@/components/functions/flows/editAction";
 import AddActionModal from "@/components/functions/flows/addAction";
+import UpdateFlowActions from "@/lib/fetch/flow/PUT/UpdateActions";
+import EditFlowActionsDetails from "@/components/functions/flows/editDetails";
 
 export default function Actions({
   flow,
@@ -26,10 +38,17 @@ export default function Actions({
   flow: any;
   runners: any;
 }) {
-  const [targetAction, setTargetAction] = React.useState(null);
+  const router = useRouter();
+  const [actions, setActions] = React.useState([] as any);
+  const [targetAction, setTargetAction] = React.useState({} as any);
+  const editFlowActionsDetails = useDisclosure();
   const addFlowActionModal = useDisclosure();
   const editActionModal = useDisclosure();
   const deleteActionModal = useDisclosure();
+
+  useEffect(() => {
+    setActions(flow.actions);
+  }, [flow]);
 
   function getActionDetails(type: string) {
     var action = {} as any;
@@ -47,6 +66,109 @@ export default function Actions({
     return action || null;
   }
 
+  const SortableItem = ({ action }: { action: any }) => {
+    const { attributes, listeners, setNodeRef, transform, transition } =
+      useSortable({ id: action.id });
+
+    const style = {
+      transform: CSS.Transform.toString(transform),
+      transition,
+    };
+
+    return (
+      <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+        <Card key={action.type} fullWidth>
+          <CardBody>
+            <div className="flex flex-cols items-center justify-between gap-2">
+              <div className="flex items-center gap-2">
+                <div className="flex bg-primary/10 text-primary items-center rounded-small justify-center w-10 h-10">
+                  <Icon icon={getActionDetails(action.type)?.icon} width={26} />
+                </div>
+                <div>
+                  <div className="flex flex-cols gap-2">
+                    <p className="text-md font-bold">
+                      {getActionDetails(action.type)?.name}
+                    </p>
+                    <Chip color="primary" radius="sm" size="sm" variant="dot">
+                      {action.id}
+                    </Chip>
+                    <Chip
+                      color={action.active ? "success" : "danger"}
+                      radius="sm"
+                      size="sm"
+                      variant="flat"
+                    >
+                      {action.active ? "Active" : "Inactive"}
+                    </Chip>
+                  </div>
+                  <p className="text-sm text-default-500">
+                    {getActionDetails(action.type)?.description}
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-cols items-center gap-2">
+                <Button
+                  isIconOnly
+                  color="warning"
+                  variant="light"
+                  onPress={() => {
+                    setTargetAction(action);
+                    editActionModal.onOpen();
+                  }}
+                >
+                  <Icon icon="solar:pen-new-square-broken" width={20} />
+                </Button>
+                <Button
+                  isIconOnly
+                  color="danger"
+                  variant="light"
+                  onPress={() => {
+                    setTargetAction(action.id);
+                    deleteActionModal.onOpen();
+                  }}
+                >
+                  <Icon icon="solar:trash-bin-2-broken" width={20} />
+                </Button>
+              </div>
+            </div>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  };
+
+  const handleDragEnd = (event: any) => {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      console.log(1);
+      const items = [...actions];
+      const oldIndex = items.findIndex((item: any) => item.id === active.id);
+      const newIndex = items.findIndex((item: any) => item.id === over.id);
+
+      const newArray = arrayMove(items, oldIndex, newIndex);
+
+      updateFlowActions(newArray);
+      setActions(newArray);
+
+      // setActions((items: any) => {
+      //   updateFlowActions(newArray);
+
+      //   return newArray;
+      // });
+    }
+  };
+
+  function updateFlowActions(items: any) {
+    UpdateFlowActions(flow.id, items)
+      .then(() => {
+        toast.success("Flow actions order updated successfully.");
+      })
+      .catch(() => {
+        toast.error("Failed to update flow actions order.");
+      });
+  }
+
   return (
     <div className="grid lg:grid-cols-3 gap-4">
       <div className="col-span-1">
@@ -59,13 +181,18 @@ export default function Actions({
                   Details which apply to all defined Actions
                 </p>
               </div>
-              <Button isIconOnly color="warning" variant="light">
+              <Button
+                isIconOnly
+                color="warning"
+                variant="light"
+                onPress={editFlowActionsDetails.onOpen}
+              >
                 <Icon icon="solar:pen-new-square-broken" width={18} />
               </Button>
             </div>
             <Divider className="my-4" />
             <div className="grid grid-cols-2 gap-4">
-              <p>Execute Actions Parallel</p>
+              <p>Execution Order</p>
               <Chip
                 color={flow.exec_parallel ? "secondary" : "primary"}
                 radius="sm"
@@ -91,6 +218,7 @@ export default function Actions({
                   {flow.patterns.map((pattern: any, index: number) => (
                     <TableRow key={index}>
                       <TableCell>{pattern.key}</TableCell>
+                      <TableCell>{pattern.type}</TableCell>
                       <TableCell>{pattern.value}</TableCell>
                     </TableRow>
                   ))}
@@ -101,42 +229,21 @@ export default function Actions({
         </Card>
       </div>
       <div className="flex flex-col gap-2 col-span-2">
-        {flow.actions.map((action: any, index: number) => (
-          <Card key={action.type} fullWidth>
-            <CardBody>
-              <div className="flex flex-cols items-center justify-between gap-2">
-                <div className="flex items-center gap-2">
-                  <div className="flex bg-primary/10 text-primary items-center rounded-small justify-center w-10 h-10">
-                    <Icon
-                      icon={getActionDetails(action.type)?.icon}
-                      width={26}
-                    />
-                  </div>
-                  <div>
-                    <div className="flex flex-cols gap-2">
-                      <p className="text-md font-bold">
-                        {getActionDetails(action.type)?.name}
-                      </p>
-                      <Chip color="primary" radius="sm" size="sm" variant="dot">
-                        {action.id}
-                      </Chip>
-                    </div>
-                    <p className="text-sm text-default-500">
-                      {getActionDetails(action.type)?.description}
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <Icon
-                    className="hover:cursor-grab"
-                    icon="nimbus:drag-dots"
-                    width={24}
-                  />
-                </div>
-              </div>
-            </CardBody>
-          </Card>
-        ))}
+        <DndContext
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+        >
+          <SortableContext
+            items={actions}
+            strategy={verticalListSortingStrategy}
+          >
+            <div className="flex flex-col gap-2">
+              {actions.map((action: any) => (
+                <SortableItem key={action.id} action={action} />
+              ))}
+            </div>
+          </SortableContext>
+        </DndContext>
         <Card
           fullWidth
           isHoverable
@@ -157,16 +264,17 @@ export default function Actions({
           </CardBody>
         </Card>
       </div>
+      <EditFlowActionsDetails disclosure={editFlowActionsDetails} flow={flow} />
       <AddActionModal
         disclosure={addFlowActionModal}
         flow={flow}
         runners={runners}
       />
       <EditActionModal
-        action={targetAction}
         disclosure={editActionModal}
-        flowID={flow.id}
+        flow={flow}
         runners={runners}
+        targetAction={targetAction}
       />
       <DeleteActionModal
         actionID={targetAction}
---
+++ lib/fetch/flow/PUT/UpdateActions.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export default async function UpdateFlowActions(flowID: string, actions: any) {
+  "use client";
+  const cookieStore = cookies();
+  const token = cookieStore.get("session")?.value;
+
+  try {
+    const headers = new Headers();
+
+    headers.append("Content-Type", "application/json");
+    if (token) {
+      headers.append("Authorization", token);
+    }
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/flows/${flowID}/actions`,
+      {
+        method: "PUT",
+        headers: headers,
+        body: JSON.stringify({
+          actions: actions,
+        }),
+      },
+    );
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    return { error: "Failed to update actions" };
+  }
+}
