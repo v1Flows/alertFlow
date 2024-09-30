efb2f8854cf2008ff811fbfca70993b881365338
JustNZ <neubert_justin@outlook.com>
2024-09-30 12:15:48 +0200

    fix: align selfhosted runner status with alertflow one

--- components/dashboard/projects/project/Runners.tsx
+++ components/dashboard/projects/project/Runners.tsx
@@ -424,8 +424,10 @@ export default function Runners({
                             />
                           </div>
                           <div>
-                            <p className="text-md font-bold">
-                              {runner.executing_job ? "Active" : "Idle"}
+                            <p
+                              className={`text-md font-bold ${runner.executing_job && "text-success"}`}
+                            >
+                              {runner.executing_job ? "Executing Job" : "Idle"}
                             </p>
                             <p className="text-sm text-default-500">Status</p>
                           </div>
