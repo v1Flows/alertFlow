04acdd8bfff531b8fb38a540cf4e63c1b86daa3e
Justin Neubert <neubert_justin@outlook.com>
2024-10-05 21:56:53 +0200

    Final version of the new Dashboard design

--- components/dashboard/executionChartCard.tsx
+++ components/dashboard/executionChartCard.tsx
@@ -13,6 +13,7 @@ import {
   Bar,
 } from "recharts";
 import { Icon } from "@iconify/react";
+import { useTheme } from "next-themes";
 
 const CustomTooltip = ({ active, payload, label }: any) => {
   if (active && payload && payload.length) {
@@ -25,8 +26,8 @@ const CustomTooltip = ({ active, payload, label }: any) => {
         padding: "10px",
         color: "#333",
       }}>
-        <p>{payload[0].value} <span className="text-default-500">Executions</span></p>
-        <p className="text-sm text-default-500">{label}</p>
+        <p className="text-sm">{payload[0].value} <span className="text-default-500">Executions</span></p>
+        <p className="text-xs text-default-500">{label}</p>
       </div>
     );
   }
@@ -36,6 +37,11 @@ const CustomTooltip = ({ active, payload, label }: any) => {
 
 export default function ExecutionChartCard({ stats }: { stats: any }) {
   const lastExecution = stats.executions[stats.executions.length - 1];
+  const { theme } = useTheme();
+
+  function barsColor() {
+    return theme === "dark" ? "#3F3F46" : "#D4D4D8";
+  }
 
   return (
     <Card>
@@ -44,7 +50,7 @@ export default function ExecutionChartCard({ stats }: { stats: any }) {
           <div className="col-span-1">
             <div className="flex items-center gap-2">
               <div className="flex bg-default/30 text-foreground items-center rounded-small justify-center w-10 h-10">
-                <Icon icon="solar:rocket-2-broken" width={20} />
+                <Icon icon="solar:reorder-line-duotone" width={20} />
               </div>
               <div>
                 <h4 className="text-large font-bold">{lastExecution.value}</h4>
@@ -53,16 +59,20 @@ export default function ExecutionChartCard({ stats }: { stats: any }) {
             </div>
           </div>
           <div className="col-span-1">
-            <div style={{ height: '80px' }}>
+            {stats.executions.filter((e: any) => e.count > 0).length > 0 ? (
+            <div style={{ height: '60px' }}>
               <ResponsiveContainer width="100%" height="100%">
                 <BarChart data={stats.executions} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                   <XAxis dataKey="date" hide={true} />
                   <YAxis hide={true} />
                   <Tooltip content={<CustomTooltip />} cursor={{ fill: 'rgba(136, 132, 216, 0.2)' }} />
-                  <Bar dataKey="count" fill="rgba(211, 211, 211, 0.6)" radius={[5, 5, 0, 0]} />
+                  <Bar dataKey="count" fill={barsColor()} radius={[5, 5, 0, 0]} />
                 </BarChart>
               </ResponsiveContainer>
             </div>
+            ) : (
+              <p className="text-sm text-default-400">No executions found for the last 7 days.</p>
+            )}
           </div>
         </div>
       </CardBody>
