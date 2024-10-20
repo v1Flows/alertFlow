fbfaa7594404182a6db5932364cd8cb0d8b647ad
Justin Neubert <neubert_justin@outlook.com>
2024-10-20 19:34:12 +0200

    Update version to 0.26.0-beta in site configuration

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "AlertFlow",
   description: "Monitoring Automation Platform.",
-  version: "0.25.0-beta",
+  version: "0.26.0-beta",
   links: {
     github: "https://github.com/AlertFlow",
     docs: "https://docs.alertflow.org",
