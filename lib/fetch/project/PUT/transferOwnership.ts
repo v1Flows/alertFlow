1b0b7cd68f456abd7b4f94fbb5dc435334a0e655
Justin Neubert <neubert_justin@outlook.com>
2024-10-20 23:25:59 +0200

    Feature: Transfer project ownership to other members

---
+++ lib/fetch/project/PUT/transferOwnership.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export default async function ProjectTransferOwnershipAPI(
+  new_owner_id: string,
+  project_id: string,
+) {
+  "use client";
+  const cookieStore = cookies();
+  const token = cookieStore.get("session")?.value;
+
+  try {
+    const headers = new Headers();
+
+    headers.append("Content-Type", "application/json");
+    if (token) {
+      headers.append("Authorization", token);
+    }
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/v1/projects/${project_id}/transfer_ownership`,
+      {
+        method: "PUT",
+        headers: headers,
+        body: JSON.stringify({
+          user_id: new_owner_id,
+        }),
+      },
+    );
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    return { error: "Failed to transfer ownership" };
+  }
+}
