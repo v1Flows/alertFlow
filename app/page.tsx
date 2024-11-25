438d0e59206755b58003fb3d165d0d8e12da2424
Justin Neubert <neubert_justin@outlook.com>
2024-11-25 20:05:37 +0100

    Feature: add FadeInImage component and update HomeShowcase layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import { Snippet, Code, Spacer } from "@nextui-org/react";
+import { Spacer } from "@nextui-org/react";
 import { cookies } from "next/headers";
 import { Icon } from "@iconify/react";
 
-import { title, subtitle } from "@/components/primitives";
 import Navbar from "@/components/navbar";
 import AdminGetSettings from "@/lib/fetch/page/settings";
 import { FeaturesSectionDemo } from "@/components/home/bento";
@@ -22,60 +21,21 @@ export default async function Home() {
   return (
     <>
       <Navbar session={session} settings={settings} user={user} />
+      <HomeShowcase />
+      <Spacer y={14} />
       <main className="container mx-auto w-full pt-2 px-6 flex-grow">
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-          <div className="z-10 flex mb-4 items-center justify-center">
-            <AnimatedGradientText>
-              🎉
-              <span
-                className={cn(
-                  `pl-2 inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
-                )}
-              >
-                Introducing AlertFlow
-              </span>
-              <Icon
-                className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5"
-                icon="solar:alt-arrow-right-broken"
-              />
-            </AnimatedGradientText>
-          </div>
-
-          <div className="inline-block text-center justify-center">
-            <h1 className={title()}>Make&nbsp;</h1>
-            <h1 className={title({ color: "blue" })}>AlertFlow&nbsp;</h1>
-            <h1 className={title()}>to your</h1>
-            <br />
-            <h1 className={title()}>personal Monitoring Assistant.</h1>
-            <h2 className={subtitle({ class: "mt-4" })}>
-              An monitoring alarm automation platform for every platform.
-            </h2>
-          </div>
-
-          <div className="mt-8">
-            <Snippet hideCopyButton hideSymbol size="lg" variant="flat">
-              <span>
-                Coming <Code color="primary">Soon.</Code>
-              </span>
-            </Snippet>
-          </div>
-
-          <div className="mt-8 w-full">
-            <HomeShowcase />
-          </div>
-        </section>
         <section>
           <FeaturesSectionDemo />
         </section>
-        <Spacer y={28} />
+        <Spacer y={14} />
         <section>
           <HomeFlows />
         </section>
-        <Spacer y={28} />
+        <Spacer y={14} />
         <section>
           <HomeRunners />
         </section>
-        <Spacer y={28} />
+        <Spacer y={14} />
         <section>
           <HomeTerraform />
         </section>
