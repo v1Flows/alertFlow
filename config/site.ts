40080780a77a4d42d7ca0b0adaf1f42253af4be0
Justin Neubert <neubert_justin@outlook.com>
2024-12-10 21:08:55 +0100

    Chore: update version to 1.0.0-beta3 in site configuration

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "AlertFlow",
   description: "Monitoring Automation Platform.",
-  version: "1.0.0-beta1",
+  version: "1.0.0-beta3",
   links: {
     github: "https://github.com/AlertFlow",
     docs: "https://docs.alertflow.org",
