268cb7ad6cbc86d60783e868011494081f6c9ce0
Justin Neubert <neubert_justin@outlook.com>
2024-12-26 00:20:18 +0100

    chore: bump version to 1.0.0-beta8 in site configuration

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "AlertFlow",
   description: "Monitoring Automation Platform.",
-  version: "1.0.0-beta7",
+  version: "1.0.0-beta8",
   links: {
     github: "https://github.com/AlertFlow",
     docs: "https://docs.alertflow.org",
