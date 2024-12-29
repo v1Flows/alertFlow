39d1e9e7d73e081d0515643fce39b1776dbe21fb
Justin Neubert <neubert_justin@outlook.com>
2024-12-29 20:15:22 +0100

    Remove unused help and profile components from mock data

--- services/frontend/components/search/mock-data.ts
+++ services/frontend/components/search/mock-data.ts
@@ -112,76 +112,10 @@ const searchData = {
       },
       content: "Security",
     },
-    {
-      slug: "profile-subscription-billing",
-      component: {
-        name: "Subscription & Billing",
-        slug: "profile-subscription-billing",
-        icon: "solar:card-2-broken",
-        attributes: {
-          group: "profile",
-          groupOrder: 3,
-          iframe: {
-            initialHeight: 220,
-            initialMobileHeight: 220,
-          },
-        },
-      },
-      url: "/profile?tab=subscription",
-      group: {
-        key: "profile",
-        name: "Profile",
-      },
-      content: "Subscription & Billing",
-    },
-    {
-      slug: "profile-quota",
-      component: {
-        name: "Quota",
-        slug: "profile-quota",
-        icon: "solar:pie-chart-2-broken",
-        attributes: {
-          group: "profile",
-          groupOrder: 4,
-          iframe: {
-            initialHeight: 220,
-            initialMobileHeight: 220,
-          },
-        },
-      },
-      url: "/profile?tab=quota",
-      group: {
-        key: "profile",
-        name: "Profile",
-      },
-      content: "Quota",
-    },
   ],
   projects: [],
   flows: [],
   help: [
-    {
-      slug: "help-documentation",
-      component: {
-        name: "Documentation",
-        slug: "help-documentation",
-        icon: "solar:notes-outline",
-        attributes: {
-          group: "informations",
-          groupOrder: 1,
-          iframe: {
-            initialHeight: 220,
-            initialMobileHeight: 220,
-          },
-        },
-      },
-      url: "/docs",
-      group: {
-        key: "informations",
-        name: "Informations",
-      },
-      content: "Documentation",
-    },
     {
       slug: "help-api",
       component: {
@@ -203,28 +137,6 @@ const searchData = {
         name: "Informations",
       },
       content: "API",
-    },
-    {
-      slug: "help-support",
-      component: {
-        name: "Support",
-        slug: "help-support",
-        icon: "solar:incoming-call-rounded-outline",
-        attributes: {
-          group: "support",
-          groupOrder: 1,
-          iframe: {
-            initialHeight: 220,
-            initialMobileHeight: 220,
-          },
-        },
-      },
-      url: "/",
-      group: {
-        key: "support",
-        name: "Support",
-      },
-      content: "Support",
     },
   ],
 };
