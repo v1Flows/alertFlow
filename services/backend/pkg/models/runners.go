f8c3b084781877ff5aec3020119bd1930bb6f9e4
Justin Neubert <neubert_justin@outlook.com>
2025-02-15 11:40:36 +0100

    refactor: change Plugins, Actions, and PayloadEndpoints fields to use slices in Runners and IncomingAutoRunners structs

--- services/backend/pkg/models/runners.go
+++ services/backend/pkg/models/runners.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"encoding/json"
 	"time"
 
 	"github.com/google/uuid"
@@ -11,33 +10,33 @@ import (
 type Runners struct {
 	bun.BaseModel `bun:"table:runners"`
 
-	ID                 uuid.UUID       `bun:",pk,type:uuid,default:gen_random_uuid()" json:"id"`
-	Name               string          `bun:"name,type:text,notnull" json:"name"`
-	Registered         bool            `bun:"registered,type:bool,default:false" json:"registered"`
-	ProjectID          string          `bun:"project_id,type:text,default:''" json:"project_id"`
-	Version            string          `bun:"version,type:text,default:''" json:"version"`
-	Mode               string          `bun:"mode,type:text,default:''" json:"mode"`
-	AutoRunner         bool            `bun:"auto_runner,type:bool,default:false" json:"auto_runner"`
-	AlertFlowRunner    bool            `bun:"alertflow_runner,type:bool,default:false" json:"alertflow_runner"`
-	LastHeartbeat      time.Time       `bun:"last_heartbeat,type:timestamptz" json:"last_heartbeat"`
-	ExecutingJob       bool            `bun:"executing_job,type:bool,default:false" json:"executing_job"`
-	Disabled           bool            `bun:"disabled,type:bool,default:false" json:"disabled"`
-	DisabledReason     string          `bun:"disabled_reason,type:text,default:''" json:"disabled_reason"`
-	Plugins            json.RawMessage `bun:"plugins,type:jsonb,default:jsonb('[]')" json:"plugins"`
-	Actions            json.RawMessage `bun:"actions,type:jsonb,default:jsonb('[]')" json:"actions"`
-	PayloadEndpoints   json.RawMessage `bun:"payload_endpoints,type:jsonb,default:jsonb('[]')" json:"payload_endpoints"`
-	RegisteredAt       time.Time       `bun:"registered_at,type:timestamptz,default:now()" json:"registered_at"`
-	ExecutedExecutions []string        `bun:"executed_executions,type:text[],default:'{}'" json:"executed_executions"`
+	ID                 uuid.UUID          `bun:",pk,type:uuid,default:gen_random_uuid()" json:"id"`
+	Name               string             `bun:"name,type:text,notnull" json:"name"`
+	Registered         bool               `bun:"registered,type:bool,default:false" json:"registered"`
+	ProjectID          string             `bun:"project_id,type:text,default:''" json:"project_id"`
+	Version            string             `bun:"version,type:text,default:''" json:"version"`
+	Mode               string             `bun:"mode,type:text,default:''" json:"mode"`
+	AutoRunner         bool               `bun:"auto_runner,type:bool,default:false" json:"auto_runner"`
+	AlertFlowRunner    bool               `bun:"alertflow_runner,type:bool,default:false" json:"alertflow_runner"`
+	LastHeartbeat      time.Time          `bun:"last_heartbeat,type:timestamptz" json:"last_heartbeat"`
+	ExecutingJob       bool               `bun:"executing_job,type:bool,default:false" json:"executing_job"`
+	Disabled           bool               `bun:"disabled,type:bool,default:false" json:"disabled"`
+	DisabledReason     string             `bun:"disabled_reason,type:text,default:''" json:"disabled_reason"`
+	Plugins            []Plugins          `bun:"plugins,type:jsonb,default:jsonb('[]')" json:"plugins"`
+	Actions            []Actions          `bun:"actions,type:jsonb,default:jsonb('[]')" json:"actions"`
+	PayloadEndpoints   []PayloadEndpoints `bun:"payload_endpoints,type:jsonb,default:jsonb('[]')" json:"payload_endpoints"`
+	RegisteredAt       time.Time          `bun:"registered_at,type:timestamptz,default:now()" json:"registered_at"`
+	ExecutedExecutions []string           `bun:"executed_executions,type:text[],default:'{}'" json:"executed_executions"`
 }
 
 type IncomingAutoRunners struct {
-	Registered       bool             `bun:"registered,type:bool,default:false" json:"registered"`
-	Version          string           `bun:"version,type:text,default:''" json:"version"`
-	Mode             string           `bun:"mode,type:text,default:''" json:"mode"`
-	LastHeartbeat    time.Time        `bun:"last_heartbeat,type:timestamptz" json:"last_heartbeat"`
-	Plugins          Plugins          `bun:"plugins,type:jsonb,default:jsonb('[]')" json:"plugins"`
-	Actions          Actions          `bun:"actions,type:jsonb,default:jsonb('[]')" json:"actions"`
-	PayloadEndpoints PayloadEndpoints `bun:"payload_endpoints,type:jsonb,default:jsonb('[]')" json:"payload_endpoints"`
+	Registered       bool               `bun:"registered,type:bool,default:false" json:"registered"`
+	Version          string             `bun:"version,type:text,default:''" json:"version"`
+	Mode             string             `bun:"mode,type:text,default:''" json:"mode"`
+	LastHeartbeat    time.Time          `bun:"last_heartbeat,type:timestamptz" json:"last_heartbeat"`
+	Plugins          []Plugins          `bun:"plugins,type:jsonb,default:jsonb('[]')" json:"plugins"`
+	Actions          []Actions          `bun:"actions,type:jsonb,default:jsonb('[]')" json:"actions"`
+	PayloadEndpoints []PayloadEndpoints `bun:"payload_endpoints,type:jsonb,default:jsonb('[]')" json:"payload_endpoints"`
 }
 
 type Plugins struct {
